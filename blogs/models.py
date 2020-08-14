6bc5cc0751d34d778bb91d6cbdf809dfa20cfdb5
三島将太 <smishima@MacBook-Pro.local>
2020-08-15 05:51:19 +0900

    create homepage

--- blogs/models.py
+++ blogs/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Blog(models.Model):
-	title = models.CharField(max_length=100)
-	pub_date = models.CharField(max_length=20)
-	body = models.CharField(max_length=200)
+	title = models.CharField(max_length=255)
+	pub_date = models.DateTimeField()
+	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
